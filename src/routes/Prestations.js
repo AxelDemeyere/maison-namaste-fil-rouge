0cf156d66bae5f8d5f941188113bd08b2fb24884
AxelDemeyere <axel.demeyere59@gmail.com>
2024-02-09 15:04:51 +0100

    🚧 Début des test de récupération des données de l'API "maison namasté"

--- src/routes/Prestations.js
+++ src/routes/Prestations.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { PresetStatusColorTypes } from "antd/es/_util/colors";
+
+
 
 function Prestations() {
+
+    const [prestations, setPrestations] = useState([]);
+
+    const getPrestations = async() => {
+        await fetch(`http://192.168.1.59:5000/prestations`)
+        .then(response => response.json())
+        .then(json => {
+            setPrestations(json);
+            console.log(json);
+        });
+    }
+
+    useEffect(() => {
+        getPrestations();
+    }, [])
+
     return (
         <>
             <Header />
+            
+            <div>
+                <h2>épilations femme</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ex rerum, necessitatibus in, voluptatum fuga doloremque quidem recusandae quam quaerat facere aspernatur. Maxime accusantium similique, nostrum officiis voluptatibus omnis dolorum.</p>
+                <p>{prestations.name}</p>
+            </div>
         </>
     )
 }

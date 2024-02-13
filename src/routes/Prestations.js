f84037fbdede9ed03cb5dea702eea07d0db9f196
Axel Demeyere <axel.demeyere59@gmail.com>
2024-02-13 15:17:27 +0100

    ✅ Page prestation OK, API Prestations & Catégories OK

--- src/routes/Prestations.js
+++ src/routes/Prestations.js
@@ -3,52 +3,49 @@ import Header from "../components/Header";
 
 function Prestations() {
 
-    const [prestations, setPrestations] = useState([]);
+
     const [categories, setCategories] = useState([]);
 
     const getCategories = async () => {
-        await fetch(`http://192.168.1.59:5000/categories`)
+        await fetch(`http://10.125.24.52:5000/categories`)
             .then(response => response.json())
             .then(json => {
                 setCategories(json);
-                console.log(json);
             })
     }
 
-    const getPrestations = async () => {
-        await fetch(`http://192.168.1.59:5000/prestations`)
-            .then(response => response.json())
-            .then(json => {
-                setPrestations(json);
-                console.log(json);
-            });
-    }
+
 
     useEffect(() => {
         getCategories();
-        getPrestations();
     }, [])
 
     return (
         <>
             <Header />
-            <div className="prestations-container">
-                {prestations.map(prestation => (
+            {categories.map(categorie => (
+                <div key={categorie._id} className="categorie-container">
+                    <h2>{categorie.name}</h2>
+                    <p>{categorie.description}</p>
+                    {categorie.prestations.map(prestation => (
+                        <div className="prestations-container">
+                            <div className="prestation">
+                                <p>{prestation.name}</p>
+                                <div className="prestation-infos">
+                                    <div className="infos-div">
+                                    <span>{prestation.price}€</span>
+                                    <span>{prestation.time}min</span>
+                                    </div>
 
-                    <div className="prestation">
-                        <p>{prestation.name}</p>
-                        <div className="prestation-infos">
-                            <span>{prestation.price}€</span><span>{prestation.time}min</span>
-                            <div className="button-div">
-                                <button>Ajouter</button>
+                                    <div className="button-div">
+                                        <button>Ajouter</button>
+                                    </div>
                                 </div>
-                            
+                            </div>
                         </div>
-                        
-                    </div>
-
-                ))}
-            </div>
+                    ))}
+                </div>
+            ))}
         </>
     )
 }

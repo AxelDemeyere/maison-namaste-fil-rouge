2f1991f48968d8acd37718db7184e51215383cfa
Axel Demeyere <axel.demeyere59@gmail.com>
2024-02-14 14:33:10 +0100

    💄 ✅ Page Contact avec lien vers tel, mail et réseau OK.

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,13 @@
 import Carousel from '../components/Carousel';
 import SearchBar from '../components/SearchBar';
 import '../stylesheets/App.css';
-import epilF from '../img/épilation.webp'
-import main from '../img/mains-pieds.jpg'
-import regard from '../img/cil.webp'
-import soinV from '../img/66-large.jpg'
-import soinC from '../img/massage.jpg'
-import epilH from '../img/épilation.webp'
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 function App() {
+
+  const navigate = useNavigate()
 
   const [categories, setCategories] = useState([]);
 
@@ -33,17 +30,15 @@ function App() {
 
       <main>
         {categories.map((categorie, index) => (
-          <div key={categorie._id} className={`container ${index % 2 === 0 ? '' : 'image-right'}`}>
+          <div key={categorie._id} className={`container ${index % 2 === 0 ? 'image-left' : 'image-right'}`}>
             <div className='img-container' >
               <img src={categorie.image} />
             </div>
             <div className="content">
               <h2>{categorie.name}</h2>
-              <p>
-                {categorie.description}
-              </p>
+              <p>{categorie.description}</p>
               <div className="button-div">
-                <button>Découvrir</button>
+                <button onClick={() => navigate(`/prestations#${categorie.name}`)}>Découvrir</button>
               </div>
             </div>
           </div>

fa426836ea282d08c952103c2af3ce3a0c11e0cf
Aurélien DAVID <aurelien.david.pro@outlook.fr>
2024-02-18 18:26:43 +0100

    changement dans les routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,17 +1,37 @@
+// App.js
+import React, { useState } from 'react';
+import { Drawer } from 'antd';
 import Carousel from '../components/Carousel';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
-import Modal from '../components/Modal';
-import '../stylesheets/App.css';
+import RegistrationForm from '../routes/RegistrationForm';
 
 function App() {
+  const [drawerVisible, setDrawerVisible] = useState(false);
+
+  const showDrawer = () => {
+    setDrawerVisible(true);
+  };
+
+  const onCloseDrawer = () => {
+    setDrawerVisible(false);
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header onMonCompteClick={showDrawer} />
       <Carousel />
       <SearchBar />
-      <Modal />
-    </div> 
+      <Drawer
+        title="Mon compte"
+        width={720}
+        onClose={onCloseDrawer}
+        visible={drawerVisible}
+        placement="right"
+      >
+        <RegistrationForm />
+      </Drawer>
+    </div>
   );
 }
 
